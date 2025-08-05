e4c28321988485dda749250c25321f23b85f2507
OtavioWM99 <otaviowerlang99@gmail.com>
2025-08-05 00:54:14 -0300

    Imprimindo mensagens no terminal

--- rust1.rs
+++ rust1.rs
@@ -1,3 +1,11 @@
+/* 
 fn main() {
     println!("Hello World")
+}
+*/
+
+fn main() {
+    println!("Hello World!"); // imprime uma mensagem simples
+    println!("Número: {}", 42); // usa {} como placeholder
+    println!("Soma: {} + {} = {}", 2, 3, 2 + 3); // múltiplos placeholders
 }
